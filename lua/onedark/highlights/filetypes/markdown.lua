dbe998323be8ab417f26bd4ea2d571710ecbef86
Jakob Helmecke <jakob.helmecke@nordlb.de>
2023-09-08 18:26:06 +0200

    feat(filetypes): add markdown

--- lua/onedark/highlights/filetypes/markdown.lua
+++ lua/onedark/highlights/filetypes/markdown.lua
@@ -4,18 +4,25 @@ local M = {}
 ---@param theme table
 ---@return table
 function M.groups(theme)
-    local config = require("onedark.config").config
+  local config = require("onedark.config").config
 
-    return {
-        ["@text.literal.markdown_inline"] = { fg = theme.palette.green },
-        ["@text.reference.markdown_inline"] = { fg = theme.palette.blue },
-        ["@text.strong.markdown_inline"] = { fg = theme.palette.orange, bold = true },
-        ["@text.title.markdown"] = { fg = theme.palette.red, bold = true },
-        ["@parameter.markdown_inline"] = { fg = theme.palette.fg },
-        ["@punctuation.special.markdown"] = { fg = theme.palette.red },
-        ["@punctuation.delimiter.markdown_inline"] = { fg = theme.palette.orange },
-        ["@text.uri.markdown_inline"] = { fg = theme.palette.purple },
-    }
+  return {
+    ["@text.title.1.markdown"] = { fg = theme.palette.green, bold = true },
+    ["@text.title.2.markdown"] = { fg = theme.palette.green, bold = true },
+    ["@text.title.3.markdown"] = { fg = theme.palette.green, bold = true },
+    ["@text.title.4.markdown"] = { fg = theme.palette.green, bold = true },
+    ["@text.title.5.markdown"] = { fg = theme.palette.green, bold = true },
+    ["@text.title.6.markdown"] = { fg = theme.palette.green, bold = true },
+    ["@text.literal.markdown_inline"] = { fg = theme.palette.yellow },
+    ["@text.reference.markdown_inline"] = { fg = theme.palette.blue },
+    ["@text.strong.markdown_inline"] = { bold = true },
+    ["@text.emphasis.markdown_inline"] = { italic = true },
+    ["@text.strike.markdown_inline"] = { strikethrough = true },
+    ["@parameter.markdown_inline"] = { fg = theme.palette.fg },
+    ["@punctuation.delimiter.markdown_inline"] = { fg = theme.palette.gray },
+    ["@text.uri.markdown_inline"] = {},
+    ["@text.todo.checked.markdown"] = { fg = theme.palette.comment },
+  }
 end
 
 return M
