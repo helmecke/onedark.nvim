3ac39d8974c8bdd29f262187d8ec1abdce76845e
Jakob Helmecke <jakob.helmecke@nordlb.de>
2024-10-10 17:19:56 +0200

    feat: enhance TreesitterContext with new highlight groups
    This commit adds new highlight groups to the TreesitterContext:
    - `TreesitterContextBottom` with underline
    - `TreesitterContextLineNumber`
    - `TreesitterContextLineNumberBottom` with underline
    
    These changes improve the visual distinction of Treesitter context lines
    and their line numbers, making it easier to navigate and understand the
    code structure.

--- lua/onedark/highlights/plugins/treesitter_context.lua
+++ lua/onedark/highlights/plugins/treesitter_context.lua
@@ -5,7 +5,10 @@ local M = {}
 ---@return table
 function M.groups(theme)
   return {
-    TreesitterContext = { bg = theme.generated.cursorline },
+    TreesitterContext = { bg = theme.generated.cursorline, fg = theme.palette.gray },
+    TreesitterContextBottom = { bg = theme.generated.cursorline, fg = theme.palette.gray, underline = true },
+    TreesitterContextLineNumber = { bg = theme.generated.cursorline, fg = theme.palette.gray },
+    TreesitterContextLineNumberBottom = { bg = theme.generated.cursorline, fg = theme.palette.gray, underline = true },
   }
 end
 
